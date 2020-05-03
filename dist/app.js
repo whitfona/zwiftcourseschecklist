5c741c1f837f545c76487c4d7613b34eae75f5e0
Nick Whitford <whitford_4@hotmail.com>
2020-05-03 13:12:20 -0400

    corrected Local Storage

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,11 @@
 const ls = new LS();
 
 document.addEventListener('DOMContentLoaded', ls.loadWatopiafromLS);
+document.addEventListener('DOMContentLoaded', ls.loadLondonfromLS);
+document.addEventListener('DOMContentLoaded', ls.loadNYCfromLS);
+document.addEventListener('DOMContentLoaded', ls.loadRichmondfromLS);
+document.addEventListener('DOMContentLoaded', ls.loadInnsbruckfromLS);
+document.addEventListener('DOMContentLoaded', ls.loadYorkshirefromLS);
 
 // make background orange when checkbox active & global counter
 const mainDiv = document.getElementById('main-div');
@@ -19,12 +24,17 @@ let yorkshireCounter = 0;
 for(let i = 0; i < boxes.length; i++) {
   if (boxes[i].type === 'checkbox') {
     boxes[i].onclick = (e) => {
+      //Check counter before adding anything in case there are saved values already increasing the count
+      counter = document.getElementById("globalOutput").innerText;
       if (boxes[i].checked === true) {
         e.toElement.parentNode.classList.add('bg-orange');
         counter++;
         
         globalOutput.innerHTML = counter;
         if (boxes[i].name === 'watopia') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          watopiaCounter = document.getElementById("watopiaOutput").innerText;
+
           watopiaCounter++;
 
           document.getElementById('watopiaOutput').innerHTML = watopiaCounter;
@@ -32,76 +42,119 @@ for(let i = 0; i < boxes.length; i++) {
           ls.addWatopiatoLS(boxes[i]);
         }
         else if (boxes[i].name === 'london') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          londonCounter = document.getElementById("londonOutput").innerText;
+
           londonCounter++;
 
           document.getElementById('londonOutput').innerHTML = londonCounter;
 
-          ls.addLondontoLS(boxes[i].checked);
+          ls.addLondontoLS(boxes[i]);
         }
         else if (boxes[i].name === 'newYork') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          newYorkCounter = document.getElementById("newYorkOutput").innerText;
+
           newYorkCounter++;
 
           document.getElementById('newYorkOutput').innerHTML = newYorkCounter;
 
-          ls.addNYCtoLS(boxes[i].checked);
+          ls.addNYCtoLS(boxes[i]);
         }
         else if (boxes[i].name === 'richmond') {
           richmondCounter++;
 
           document.getElementById('richmondOutput').innerHTML = richmondCounter;
 
-          ls.addRichmondtoLS(boxes[i].checked);
+          ls.addRichmondtoLS(boxes[i]);
         }
         else if (boxes[i].name === 'innsbruck') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          innsbruckCounter = document.getElementById("innsbruckOutput").innerText;
+
           innsbruckCounter++;
 
           document.getElementById('innsbruckOutput').innerHTML = innsbruckCounter;
 
-          ls.addInnsbrucktoLS(boxes[i].checked);
+          ls.addInnsbrucktoLS(boxes[i]);
         }
         else if (boxes[i].name === 'yorkshire') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          yorkshireCounter = document.getElementById("yorkshireOutput").innerText;
+
           yorkshireCounter++;
 
           document.getElementById('yorkshireOutput').innerHTML = yorkshireCounter;
 
-          ls.addYorkshiretoLS(boxes[i].checked);
+          ls.addYorkshiretoLS(boxes[i]);
         }
       } else {
+        //Check counter before adding anything in case there are saved values already increasing the count
+        counter = document.getElementById("globalOutput").innerText;
+
         e.toElement.parentNode.classList.remove('bg-orange');
         counter--;
   
         globalOutput.innerHTML = counter;
         if (boxes[i].name === 'watopia') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          watopiaCounter = document.getElementById("watopiaOutput").innerText;
+
           watopiaCounter--;
 
           document.getElementById('watopiaOutput').innerHTML = watopiaCounter;
 
-          ls.removeWatopiafromLS(boxes[i])
+          ls.removeWatopiafromLS(boxes[i]);
         }
         else if (boxes[i].name === 'london') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          londonCounter = document.getElementById("londonOutput").innerText;
+
           londonCounter--;
 
           document.getElementById('londonOutput').innerHTML = londonCounter;
+
+          ls.removeLondonfromLS(boxes[i]);
         }
         else if (boxes[i].name === 'newYork') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          newYorkCounter = document.getElementById("newYorkOutput").innerText;
+
           newYorkCounter--;
 
           document.getElementById('newYorkOutput').innerHTML = newYorkCounter;
+
+          ls.removeNYCfromLS(boxes[i]);
         }
         else if (boxes[i].name === 'richmond') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          richmondCounter = document.getElementById("richmondOutput").innerText;
+
           richmondCounter--;
 
           document.getElementById('richmondOutput').innerHTML = richmondCounter;
+
+          ls.removeRichmondfromLS(boxes[i]);
         }
         else if (boxes[i].name === 'innsbruck') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          innsbruckCounter = document.getElementById("innsbruckOutput").innerText;
+
           innsbruckCounter--;
 
           document.getElementById('innsbruckOutput').innerHTML = innsbruckCounter;
+
+          ls.removeInnsbruckfromLS(boxes[i]);
         }
         else if (boxes[i].name === 'yorkshire') {
+          //Check counter before adding anything in case there are saved values already increasing the count
+          yorkshireCounter = document.getElementById("yorkshireOutput").innerText;
+
           yorkshireCounter--;
 
           document.getElementById('yorkshireOutput').innerHTML = yorkshireCounter;
+
+          ls.removeYorkshirefromLS(boxes[i]);
         }
       }
     };
--- dist/localStorage.js
+++ dist/localStorage.js
@@ -1,7 +1,4 @@
 class LS {
-  constructor() {
-  }
-
   // WATOPIA
   addWatopiatoLS(watopia) {
     let watopiaList;
@@ -35,6 +32,9 @@ class LS {
 
   loadWatopiafromLS() {
     let watopiaList;
+     //Get counter values
+     let watopiaOutput = document.getElementById("watopiaOutput").innerText;
+     let globalOutput = document.getElementById("globalOutput").innerText;
     if(localStorage.getItem('watopia') === null) {
       watopiaList = [];
     } else {
@@ -42,8 +42,16 @@ class LS {
     }
     
     watopiaList.forEach(function(item) {
-    //  NOT SURE WHAT TO PUT IN HERE. I CAN HAVE THE PAGE LOAD WITH THE BOXES STILL CHECKED BUT THEY DON'T GET INCLUDED IN THE COUNTERS AND THEY ARE NOT ORANGE LIKE BEFORE PUT INTO LOCAL STORAGE
+      //Set check boxes to checked for all the elements in watopiaList and apply the orange class the checkbox parent
+      document.getElementById(item).checked = true;
+      document.getElementById(item).parentNode.classList.add('bg-orange');
+      //Increase the counters by one for each item
+      watopiaOutput++;
+      globalOutput++;
     })
+
+    document.getElementById("watopiaOutput").innerText = watopiaOutput;
+    document.getElementById("globalOutput").innerText = globalOutput;
   }
 
    // LONDON
@@ -54,10 +62,51 @@ class LS {
     } else {
       londonList = JSON.parse(localStorage.getItem('london'));
     }
-
-    londonList.push(london);
+    
+    londonList.push(london.id);
 
     localStorage.setItem('london', JSON.stringify(londonList));
+  }
+
+  removeLondonfromLS(item) {
+    let londonList;
+    if(localStorage.getItem('london') === null) {
+      londonList = [];
+    } else {
+      londonList = JSON.parse(localStorage.getItem('london'));
+    }
+
+    londonList.forEach(function(londonItem, index) {
+      if(item.id === londonItem) {
+        londonList.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('london', JSON.stringify(londonList));
+  }
+
+  loadLondonfromLS() {
+    let londonList;
+     //Get counter values
+     let londonOutput = document.getElementById("londonOutput").innerText;
+     let globalOutput = document.getElementById("globalOutput").innerText;
+    if(localStorage.getItem('london') === null) {
+      londonList = [];
+    } else {
+      londonList = JSON.parse(localStorage.getItem('london'));
+    }
+    
+    londonList.forEach(function(item) {
+      //Set check boxes to checked for all the elements in londonList and apply the orange class the checkbox parent
+      document.getElementById(item).checked = true;
+      document.getElementById(item).parentNode.classList.add('bg-orange');
+      //Increase the counters by one for each item
+      londonOutput++;
+      globalOutput++;
+    })
+
+    document.getElementById("londonOutput").innerText = londonOutput;
+    document.getElementById("globalOutput").innerText = globalOutput;
   }
 
    // NEW YORK
@@ -69,12 +118,53 @@ class LS {
       nycList = JSON.parse(localStorage.getItem('nyc'));
     }
 
-    nycList.push(nyc);
+    nycList.push(nyc.id);
 
     localStorage.setItem('nyc', JSON.stringify(nycList));
   }
 
-  // NEW YORK
+  removeNYCfromLS(item) {
+    let nycList;
+    if(localStorage.getItem('nyc') === null) {
+      nycList = [];
+    } else {
+      nycList = JSON.parse(localStorage.getItem('nyc'));
+    }
+
+    nycList.forEach(function(nycItem, index) {
+      if(item.id === nycItem) {
+        nycList.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('nyc', JSON.stringify(nycList));
+  }
+
+  loadNYCfromLS() {
+    let nycList;
+     //Get counter values
+     let nycOutput = document.getElementById("newYorkOutput").innerText;
+     let globalOutput = document.getElementById("globalOutput").innerText;
+    if(localStorage.getItem('nyc') === null) {
+      nycList = [];
+    } else {
+      nycList = JSON.parse(localStorage.getItem('nyc'));
+    }
+    
+    nycList.forEach(function(item) {
+      //Set check boxes to checked for all the elements in nycList and apply the orange class the checkbox parent
+      document.getElementById(item).checked = true;
+      document.getElementById(item).parentNode.classList.add('bg-orange');
+      //Increase the counters by one for each item
+      nycOutput++;
+      globalOutput++;
+    })
+
+    document.getElementById("newYorkOutput").innerText = nycOutput;
+    document.getElementById("globalOutput").innerText = globalOutput;
+  }
+
+  // RICHMOND
   addRichmondtoLS(richmond) {
   let richmondList;
   if(localStorage.getItem('richmond') === null) {
@@ -83,9 +173,50 @@ class LS {
     richmondList = JSON.parse(localStorage.getItem('richmond'));
   }
 
-  richmondList.push(richmond);
+  richmondList.push(richmond.id);
 
   localStorage.setItem('richmond', JSON.stringify(richmondList));
+  }
+
+  removeRichmondfromLS(item) {
+    let richmondList;
+    if(localStorage.getItem('richmond') === null) {
+      richmondList = [];
+    } else {
+      richmondList = JSON.parse(localStorage.getItem('richmond'));
+    }
+
+    richmondList.forEach(function(richmondItem, index) {
+      if(item.id === richmondItem) {
+        richmondList.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('richmond', JSON.stringify(richmondList));
+  }
+
+  loadRichmondfromLS() {
+    let richmondList;
+     //Get counter values
+     let richmondOutput = document.getElementById("richmondOutput").innerText;
+     let globalOutput = document.getElementById("globalOutput").innerText;
+    if(localStorage.getItem('richmond') === null) {
+      richmondList = [];
+    } else {
+      richmondList = JSON.parse(localStorage.getItem('richmond'));
+    }
+    
+    richmondList.forEach(function(item) {
+      //Set check boxes to checked for all the elements in richmondList and apply the orange class the checkbox parent
+      document.getElementById(item).checked = true;
+      document.getElementById(item).parentNode.classList.add('bg-orange');
+      //Increase the counters by one for each item
+      richmondOutput++;
+      globalOutput++;
+    })
+
+    document.getElementById("richmondOutput").innerText = richmondOutput;
+    document.getElementById("globalOutput").innerText = globalOutput;
   }
   
   // INNSBRUCK
@@ -97,12 +228,53 @@ class LS {
     innsbruckList = JSON.parse(localStorage.getItem('innsbruck'));
   }
 
-  innsbruckList.push(innsbruck);
+  innsbruckList.push(innsbruck.id);
 
   localStorage.setItem('innsbruck', JSON.stringify(innsbruckList));
   }
 
-  // INNSBRUCK
+  removeInnsbruckfromLS(item) {
+    let innsbruckList;
+    if(localStorage.getItem('innsbruck') === null) {
+      innsbruckList = [];
+    } else {
+      innsbruckList = JSON.parse(localStorage.getItem('innsbruck'));
+    }
+
+    innsbruckList.forEach(function(innsbruckItem, index) {
+      if(item.id === innsbruckItem) {
+        innsbruckList.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('innsbruck', JSON.stringify(innsbruckList));
+  }
+
+  loadInnsbruckfromLS() {
+    let innsbruckList;
+     //Get counter values
+     let innsbruckOutput = document.getElementById("innsbruckOutput").innerText;
+     let globalOutput = document.getElementById("globalOutput").innerText;
+    if(localStorage.getItem('innsbruck') === null) {
+      innsbruckList = [];
+    } else {
+      innsbruckList = JSON.parse(localStorage.getItem('innsbruck'));
+    }
+    
+    innsbruckList.forEach(function(item) {
+      //Set check boxes to checked for all the elements in innsbruckList and apply the orange class the checkbox parent
+      document.getElementById(item).checked = true;
+      document.getElementById(item).parentNode.classList.add('bg-orange');
+      //Increase the counters by one for each item
+      innsbruckOutput++;
+      globalOutput++;
+    })
+
+    document.getElementById("innsbruckOutput").innerText = innsbruckOutput;
+    document.getElementById("globalOutput").innerText = globalOutput;
+  }
+
+  // YORKSHIRE
   addYorkshiretoLS(yorkshire) {
     let yorkshireList;
     if(localStorage.getItem('yorkshire') === null) {
@@ -111,8 +283,49 @@ class LS {
       yorkshireList = JSON.parse(localStorage.getItem('yorkshire'));
     }
   
-    yorkshireList.push(yorkshire);
+    yorkshireList.push(yorkshire.id);
   
     localStorage.setItem('yorkshire', JSON.stringify(yorkshireList));
+    }
+
+    removeYorkshirefromLS(item) {
+      let yorkshireList;
+      if(localStorage.getItem('yorkshire') === null) {
+        yorkshireList = [];
+      } else {
+        yorkshireList = JSON.parse(localStorage.getItem('yorkshire'));
+      }
+  
+      yorkshireList.forEach(function(yorkshireItem, index) {
+        if(item.id === yorkshireItem) {
+          yorkshireList.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('yorkshire', JSON.stringify(yorkshireList));
+    }
+  
+    loadYorkshirefromLS() {
+      let yorkshireList;
+       //Get counter values
+       let yorkshireOutput = document.getElementById("yorkshireOutput").innerText;
+       let globalOutput = document.getElementById("globalOutput").innerText;
+      if(localStorage.getItem('yorkshire') === null) {
+        yorkshireList = [];
+      } else {
+        yorkshireList = JSON.parse(localStorage.getItem('yorkshire'));
+      }
+      
+      yorkshireList.forEach(function(item) {
+        //Set check boxes to checked for all the elements in yorkshireList and apply the orange class the checkbox parent
+        document.getElementById(item).checked = true;
+        document.getElementById(item).parentNode.classList.add('bg-orange');
+        //Increase the counters by one for each item
+        yorkshireOutput++;
+        globalOutput++;
+      })
+  
+      document.getElementById("yorkshireOutput").innerText = yorkshireOutput;
+      document.getElementById("globalOutput").innerText = globalOutput;
     }
 }   
